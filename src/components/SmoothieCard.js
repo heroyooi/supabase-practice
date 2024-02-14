dd24a0f5085150db5abc4aa0b43b7daeb8283eaf
heroyooi <heoryooi1018@gmail.com>
2024-02-14 10:59:33 +0900

    #8 Updating Local State

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,18 +1,21 @@
 import { Link } from 'react-router-dom'
 import supabase from '../config/supabaseClient'
 
-const SmoothieCard = ({ smoothie }) => {
+const SmoothieCard = ({ smoothie, onDelete }) => {
   const handleDelete = async () => {
     const { data, error } = await supabase
       .from('smoothies')
       .delete()
       .eq('id', smoothie.id)
+      .select()
+
 
     if (error) {
       console.log(error)
     }
     if (data) {
       console.log(data)
+      onDelete(smoothie.id)
     }
   }
 
